1fa885bca2143d435d8d3e52bc3e37683ac9cb10
shira <shira2003shira@gmail.com>
2026-02-23 22:10:54 +0200

    Remove unused file

--- client/src/hooks/useToken.ts
+++
@@ -1,8 +0,0 @@
-
-export const accessTokenAtom = atom<string | null>(null);
-
-export const useToken = () => {
-  const [accessToken, setAccessToken] = useAtom(accessTokenAtom);
-
-  return { accessToken, setAccessToken };
-};
